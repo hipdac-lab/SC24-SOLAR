b8e1f5197546748346e1364e88e81ded29e00555
Baixi Sun <sunbaixi@163.com>
2023-08-04 15:50:57 -0400

    Update run_end2end.sh

--- Cosmoflow/run_end2end.sh
+++ Cosmoflow/run_end2end.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 'Running Baseline Training'
 #mpirun -np ${NPROCS} python3 train_baseline.py --data_path ../../data/cosmoUniverse_21688988_128GB_v1 --batch_size 16 --nepochs 3 --nsamples $((MY_SIZE*51))
-mpirun -np 2 python3 train_baseline.py --data_path ../../data/cosmoUniverse_21688988_128GB_v1 --batch_size 16 --nepochs 3 --nsamples 32
+mpirun -np 2 python3 train_baseline.py --data_path ../../data/cosmoUniverse_21688988_128GB_v1 --batch_size 8 --nepochs 3 --nsamples 32
 wait
 echo 'Running SOLAR shuffle'
-python3 ../utils/solar_shuffle.py --size 4 --gpu_pernode 1 --batch_size 16 --nnodes 4 --epochs 3 --cache_size 16 --ntrain 32 --save_path ./lists/
+python3 ../utils/solar_shuffle.py --size 4 --gpu_pernode 1 --batch_size 16 --nnodes 4 --epochs 3 --cache_size 8 --ntrain 32 --save_path ./lists/
 wait
 echo 'Running SOLAR Training'
-mpirun -np 2 python3 train_solar.py --data_path ../../data/cosmoUniverse_21688988_128GB_v1 --batch_size 16 --nepochs 3 --nsamples 32 --buffer_size 16 --lists ./lists/
+mpirun -np 2 python3 train_solar.py --data_path ../../data/cosmoUniverse_21688988_128GB_v1 --batch_size 8 --nepochs 3 --nsamples 32 --buffer_size 16 --lists ./lists/
